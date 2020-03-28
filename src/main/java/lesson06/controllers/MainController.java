859c18bfeb8a7b4f9c018f18565b8cf1afba58ec
ligovean <ligomail@rambler.ru>
2020-03-28 17:03:57 +0300

    Lesson06

--- src/main/java/lesson06/controllers/MainController.java
+++ src/main/java/lesson06/controllers/MainController.java
@@ -1,9 +1,7 @@
 package lesson06.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
--- src/main/java/lesson06/launchers/SecurityConfig.java
+++ src/main/java/lesson06/launchers/SecurityConfig.java
@@ -38,24 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .withUser(users.username("user2").password("$2a$10$Ky8sAV0Zbj8RKJcCozhkZuR7S8pIT2Sif/1v42V26eTzx3lzhuQQO").roles("USER"));
     }
 
-
     protected void configure(HttpSecurity http) throws Exception {
-        //     http.csrf().ignoringAntMatchers("/**");
-
-//        http.authorizeRequests()
-//                .anyRequest().authenticated()
-//                .antMatchers("/shop/**").permitAll()
-//                .antMatchers("/admin/**").hasAnyRole("MANAGER", "ADMIN")
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .logout()
-//                    .deleteCookies("JSESSIONID")
-//                    .logoutUrl("/exit")
-//                    .logoutSuccessUrl("/shop");
-
-
-
         http.authorizeRequests()
                 .antMatchers("/").hasAnyRole("USER")
                 .antMatchers("/admin/**").hasRole("ADMIN")
@@ -64,51 +47,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .loginProcessingUrl("/authenticateTheUser")
                 .permitAll();
-
-//        http.authorizeRequests()
-////                .anyRequest().authenticated()
-//                .antMatchers("/").permitAll()
-//                .antMatchers("/admin").hasRole("ADMIN")
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                // .failureUrl("/ohSh")
-//                .loginProcessingUrl("/authenticateTheUser")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-
-// 1
-
-//        http.authorizeRequests()
-//                .anyRequest().authenticated()
-//            //    .antMatchers("/*").hasAnyRole("ADMIN","USER");
-//                .antMatchers("/").hasAnyRole("USER")
-//                .antMatchers("/admin/**").hasRole("ADMIN")
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")
-//                // .failureUrl("/ohSh")
-//                .loginProcessingUrl("/authenticateTheUser")
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll()
-//                .and()
-//                .exceptionHandling().accessDeniedPage("/accessDenied");
-
-
-//        http
-//                .csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/").hasRole("USER")
-//                .anyRequest().authenticated()
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
     }
 
 }
